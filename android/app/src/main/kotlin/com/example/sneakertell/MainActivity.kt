59ad707ef7905f5c4a348171046d9f92087755d1
ishtiaqmahmood <ishtiaqmahmood45@gmail.com>
2026-06-18 16:31:58 +0600

    feat: Shop and Cart bottomnavigation added

---
+++ android/app/src/main/kotlin/com/example/sneakertell/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sneakertell
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:sneakertell/components/bottom_nav_bar.dart';
+import 'package:sneakertell/pages/cart_page.dart';
+import 'package:sneakertell/pages/shop_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // this selected index is to control the bottom nav bar
+  int _selectedIndex = 0;
+
+  // this method will update our selected index
+  // when the user taps on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // page to display
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+    // cart page
+    const CartPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
